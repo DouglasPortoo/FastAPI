8de2167181f6fc12f2521fc39510c09c7f94d808
Douglas Porto <portogdouglas@gmail.com>
2026-03-06 16:26:22 -0300

    feat: add suporte a autenticação de usuário com JWT

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,9 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from models import User
 from models import db
+from fastapi import Depends, HTTPException
+from jose import JWTError, jwt
+from main import SECRET_KEY, ALGORITHM,oauth2_scheme
 
 def getSession():
   try:
@@ -8,3 +12,16 @@ def getSession():
     yield session
   finally:
     session.close()
+
+def verify_token(token: str = Depends(oauth2_scheme), SESSION: Session = Depends(getSession)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = int(payload.get("sub"))
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Token inválido")
+        user = SESSION.query(User).filter(User.id == user_id).first()
+        if user is None:
+            raise HTTPException(status_code=401, detail="Token inválido")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token inválido")
